81e70e1338f595340d337711caa1cdc1c3f89de7
Mike Jacobi <mikerjacobi@gmail.com>
2012-07-22 16:37:32 -0700

    makeusers changes

--- makeusers.sh
+++ makeusers.sh
@@ -8,19 +8,19 @@ sudo usermod -a -G group$1 moderator
 sudo mkdir /home/moderator/pipes/$1'tosD'
 sudo chown group$1 /home/moderator/pipes/$1'tosD'
 sudo chgrp group$1 /home/moderator/pipes/$1'tosD'
-sudo chmod 240 /home/moderator/pipes/$1'tosD'
+sudo chmod 350 /home/moderator/pipes/$1'tosD'
 
 #create directory for server to user communication
 sudo mkdir /home/moderator/pipes/'sto'$1'D'
 sudo chown group$1 /home/moderator/pipes/'sto'$1'D'
 sudo chgrp group$1 /home/moderator/pipes/'sto'$1'D'
-sudo chmod 420 /home/moderator/pipes/'sto'$1'D'
+sudo chmod 530 /home/moderator/pipes/'sto'$1'D'
 
 #create respective fifos
 sudo mkfifo /home/moderator/pipes/sto$1D/sto$1
-sudo chmod 420 /home/moderator/pipes/sto$1D/sto$1
+sudo chmod 530 /home/moderator/pipes/sto$1D/sto$1
 sudo mkfifo /home/moderator/pipes/$1tosD/$1tos
-sudo chmod 240 /home/moderator/pipes/$1tosD/$1tos
+sudo chmod 350 /home/moderator/pipes/$1tosD/$1tos
 
 #setup fifo perms
 sudo chown group$1 /home/moderator/pipes/sto$1D/sto$1
