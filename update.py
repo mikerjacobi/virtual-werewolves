c9825f884d466667664597447217a841667bed50
Xu Zhang <zhangxu1115@gmail.com>
2012-07-27 14:00:14 -0700

    update GPL license for all files, fix mkdir bug in install.sh,still need update README

--- update.py
+++ update.py
@@ -1,3 +1,24 @@
+#Author: Mike Jacobi
+#Test and Update: Xu Zhang
+#Virtual Werewolf Game
+#Instructors: Roya Ensafi, Jed Crandall
+#Cybersecurity, Spring 2012
+
+#Copyright (c) 2012 Mike Jacobi, Xu Zhang, Roya Ensafi, Jed Crandall
+#This file is part of Virtual Werewolf Game.
+
+#Virtual werewolf is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#Virtual werewolf is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Virtual werewolf.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 for i in range(1,16):
